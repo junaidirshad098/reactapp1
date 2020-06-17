a666e25c8dd243058b66562c96b5334f2c3b02e8
Muhammad Junaid <junaidirshad098@gmail.com>
2020-06-18 03:28:34 +0500

    new dinner file is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Fun1 from './dinner.js';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
      <h1>Secrets token create new it can be error </h1>
+     <Fun1 foodname="Chicken"/>
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Fun1(props)
+{
+
+return(
+
+<h1>
+Today we are serving {props.foodname}
+</h1>
+
+	)
+
+}
+export default Fun1;

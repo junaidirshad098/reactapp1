279a773b21d8ea0d6c0fdb04808e40231f57c2c6
Muhammad Junaid <junaidirshad098@gmail.com>
2020-06-18 23:58:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Fun1 from './dinner.js';
 
 function App() {
+
+let [count,setCount]=useState(0);
+
   return (
     <div className="App">
      <h1>Hello world</h1>
      <h1>Secrets token create new it can be error </h1>
      <Fun1 foodname="Chicken"/>
+
+<H1>Counter ++ =:{count}<h1>
+     <Button onClick={()=>setCount(count++)}>
+Update Count
+     </Button>
     </div>
   );
 }

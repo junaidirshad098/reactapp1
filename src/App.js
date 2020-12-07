dbe16b0f2becb0fddb5971fe42c6512f70f5eb5f
Muhammad Junaid <junaidirshad098@gmail.com>
2020-12-07 21:53:56 +0500

    class4#counter-day-night

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React,{useState} from 'react';
 import './App.css';
 import Fun1 from './dinner.js';
 
-function App() {
+export default function App() {
 
 let [count,setCount]=useState(0);
+let [count1,setCount1]=useState(0);
+let [isMorning,setMorning]=useState(false);
 
   return (
-    <div className="App">
+    // <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+    
+    <div className={` ${isMorning ? ' day-night'  :  'day-morning ' }`} >
      <h1>Hello world</h1>
      <h1>Secrets token my create new it can be error </h1>
      <Fun1 foodname="Chicken"/>
 
-<h1>Counter ++ =:{count}</h1>
-     <button onClick={()=>setCount(count++)}>
-Update Count
+<h1>++Counter =:{count}</h1>
+     <button onClick={()=>setCount(++count)}>
+Update ++Count
      </button>
+     <h2>Counter++=:{count1}</h2>
+     <button onClick={()=>setCount1(count1++)}>Count++</button>
+     <br></br>
+  <button onClick={()=>setMorning(!isMorning)}>{isMorning? "Morning":"Night"}</button>
     </div >
   );
+
+
+
 }
 
-export default App;
+// export default App;
--- src/dinner.js
+++ src/dinner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css';
 function Fun1(props)
 {
 
 return(
 
-<h1>
+<h1 >
 Today we are serving 1 {props.foodname}
 </h1>
 
